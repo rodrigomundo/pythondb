fd83e99183ea96353f6119699e11d5ee1fcb1cb7
rodrigomundo <rodrigo.mundo4649@alumnos.udg.mx>
2024-01-31 16:35:59 -0800

    Update BASEDEDATOS.py
    AJA

--- BASEDEDATOS.py
+++ BASEDEDATOS.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
-#Oliwis
 
 import pyodbc
 SERVER = 'LAPTOP-0DFDCEIG'
@@ -20,10 +19,18 @@ class clasesilla:
         self.cursor.execute(sqlstatement)
         resultado = self.cursor.fetchall()
         return resultado
+    def recivalores (self, sql, vals):
+        self.cursor.execute(sql, vals)
+        resultado = self.cursor.fetchall()
+        return resultado
         
             
 objeto = clasesilla()
+valores = (4, 5)
+resultado3 = objeto.recivalores ("EXECUTE [dbo].[un_storprosellur] ?, ?", valores)
 resultado2 = objeto.micursorcito ("SELECT ProductID, Name, Culture FROM SalesLT.vProductAndDescription")
+for x in resultado3:
+            print (x[0], "-", x[1], "-", x[2], "-", x[4], "-", x[5])
 for x in resultado2:
             print (x[0], "-", x[1])
 objeto.micursorcito ("SELECT * FROM SalesLT.vGetAllCategories")
