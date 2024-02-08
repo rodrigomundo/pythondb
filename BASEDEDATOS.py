a349da3697c1799be38695eaaecd070ca5738c20
rodrigomundo <rodrigo.mundo4649@alumnos.udg.mx>
2024-02-08 12:32:26 -0800

    Update BASEDEDATOS.py
    OK

--- BASEDEDATOS.py
+++ BASEDEDATOS.py
@@ -1,39 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
-import pyodbc
+import sys
+sys.path.append(r"C:\Users\rodri\Documents\GitHub\pythondb")
+
+from LIBRERIACLASE import clasesilla
+
 SERVER = 'LAPTOP-0DFDCEIG'
 DATABASE = 'AdventureWorksLT2019'
 USERNAME = 'rod'
 PASSWORD = 'Borges2017'
-class clasesilla:
-    def __init__(self):
-        connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}; Encrypt=no;TrustServerCertificate=yes'
-        self.conn = pyodbc.connect(connectionString) 
-        self.cursor = self.conn.cursor()
-    def micursorcito(self, sqlstatement):
-        #query = "SELECT ProductID, Name, Culture FROM SalesLT.vProductAndDescription"
-        self.cursor.execute(sqlstatement)
-        resultado = self.cursor.fetchall()
-        return resultado
-    def recivalores (self, sql, vals):
-        self.cursor.execute(sql, vals)
-        resultado = self.cursor.fetchall()
-        return resultado
-        
-            
-objeto = clasesilla()
-valores = (4, 5)
-resultado3 = objeto.recivalores ("EXECUTE [dbo].[un_storprosellur] ?, ?", valores)
-resultado2 = objeto.micursorcito ("SELECT ProductID, Name, Culture FROM SalesLT.vProductAndDescription")
-for x in resultado3:
-            print (x[0], "-", x[1], "-", x[2], "-", x[4], "-", x[5])
-for x in resultado2:
-            print (x[0], "-", x[1])
-objeto.micursorcito ("SELECT * FROM SalesLT.vGetAllCategories")
+#objeto = clasesilla(SERVER, DATABASE, USERNAME, PASSWORD)
+#valores = (4, 5)
+#resultado3 = objeto.recivalores ("EXECUTE [dbo].[un_storprosellur] ?, ?", valores)
+#resultado2 = objeto.micursorcito ("SELECT ProductID, Name, Culture FROM SalesLT.vProductAndDescription")
+#for x in resultado3:
+#           print (x[0], "-", x[1], "-", x[2], "-", x[4], "-", x[5])
+#for x in resultado2:
+#           print (x[0], "-", x[1])
+#objeto.micursorcito ("SELECT * FROM SalesLT.vGetAllCategories")
+
+objeto2 = clasesilla(SERVER, 'MUSIKA', USERNAME, PASSWORD)
+valores2 = ('Hammer smashed face', 2)
+resultado4 = objeto2.insertar ("EXEC [dbo].[CreaRola] ?, ?", valores2)
 
 
 # In[ ]:
