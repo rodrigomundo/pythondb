5da1d952a66b83ab63926a329cf2a77d55d7b803
rodrigomundo <rodrigo.mundo4649@alumnos.udg.mx>
2024-02-19 13:31:29 -0800

    cambio
    cambio

--- LIBRERIACLASE.py
+++ LIBRERIACLASE.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import pyodbc
+import requests
+import pandas as pd
+import json
+from pandas.io.json import json_normalize
 class clasesilla:
     def __init__(self, SERVER, DATABASE, USERNAME, PASSWORD):
         connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}; Encrypt=no;TrustServerCertificate=yes'
@@ -24,8 +28,13 @@ class clasesilla:
         self.cursor.commit()
         self.cursor.close()
         return 0
-        
-            
+    
+class fruta:
+    def getapi (self, apiurl):
+        df = pd.read_json(apiurl)
+        return df
+    def putapi (self, apiurl, payload):
+        posti = requests.put(apiurl, data=payload)
 
 
 # In[ ]:
